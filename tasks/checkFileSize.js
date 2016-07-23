f51a1b35c70b3c07ba4222f2c583e0eca56d9b46
Danyal Ali Butt <danyal@hotmail.dk>
2016-07-23 09:48:01 +0200

    single task plugin

--- tasks/checkFileSize.js
+++ tasks/checkFileSize.js
@@ -8,43 +8,52 @@
 
 'use strict';
 
+var fs = require('fs');
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
+  function dumpDebugInformation(options) {
+    if (options.debug !== undefined) {
+      grunt.log.writeflags(options, 'Options');
+    }
+  }
 
-  grunt.registerMultiTask('checkFileSize', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+  function verifyFolderExists(folderPath) {
+    if (folderPath === "" || folderPath === undefined) {
+      grunt.fail.fatal("The provided folderToScan was empty or not provided");
+    }
+    if (!grunt.file.exists(folderPath)) {
+      grunt.fail.fatal("The provided folderToScan was not found");
+    }
+    if (!grunt.file.isDir(folderPath)) {
+      grunt.fail.fatal("The provided folderToScan was not a folder");
+    }
+  }
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+  function checkFileSizes(options) {
+    grunt.file.recurse(options.folderToScan, function(abspath, rootdir, subdir, filename) {
+        if (grunt.file.isFile(abspath)) {
+          var stats = fs.statSync(abspath);
+          var asBytes = stats.size / 1024;
+          grunt.log.writeln("Found file %s with size of %s kb", filename, asBytes);
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
     });
+  }
+
+  grunt.registerTask('checkFileSize', 'The best Grunt plugin ever.', function() {
+    var options = this.options({
+      folderToScan : './tasks',
+      debug: false
+    });
+
+    // debug
+    dumpDebugInformation(options);
+    // validation
+    verifyFolderExists(options.folderToScan);
+    // action
+    checkFileSizes(options);
+
   });
 
 };
